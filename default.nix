5c760e863ff084c99140eeb5094f89e922eb302b
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-03-14 20:34:59 +0800

    Created wrapper script for mysc-server

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
 { pkgs ? import <nixpkgs> {config = (import ./config.nix) pkgs "ghc802" "ghcjsHEAD";}
 }:
-
-let fix = f: let result = f result; in result;
-in pkgs.hpkgs.mysc-server
+let static-dir = pkgs.runCommand "static-dir" {} ''
+  mkdir $out
+  ${pkgs.hpkgs.mysc-common}/bin/mysc-html > $out/index.html
+  cp ${pkgs.hjspkgs.mysc-client}/all.min.js $out/all.min.js
+  '';
+in
+pkgs.writeScriptBin "run-mysc-server" ''
+  #!${pkgs.stdenv.shell}
+  ${pkgs.hpkgs.mysc-server}/bin/mysc-server -p 8080 -d ${static-dir}/
+''
