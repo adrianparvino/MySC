09b1496b9dbe033e03819ebf6baf034c64f2c633
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-03-14 19:40:09 +0800

    Changed dependency graph

--- default.nix
+++ default.nix
@@ -2,22 +2,4 @@
 }:
 
 let fix = f: let result = f result; in result;
-in
-pkgs.stdenv.mkDerivation {
-  name = "mysc";
-  version = "0.1.0.0";
-
-  phases = [ "installPhase" ];
-
-  installPhase = ''
-    true
-    mkdir -p $out/bin/
-    cp ${pkgs.hpkgs.mysc-server}/bin/heroku-comment-server $out/bin/
-    cp ${pkgs.hjspkgs.mysc-client}/index.html $out/
-    cp ${pkgs.hjspkgs.mysc-client}/all.min.js $out/
-  '';
-  
-  meta = {
-    license = pkgs.stdenv.lib.licenses.gpl3;
-  };
-}
+in pkgs.hpkgs.mysc-server
